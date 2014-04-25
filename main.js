2815adeae94d879764bf096db7473a127aa54f3a
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-04-25 15:33:28 -0400

    Realized the basic functionality

--- main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $('h1').click(DemoApp.changeToRed);
-  $(window).resize(DemoApp.soundAlarm);
-  $('#fade-control').click(DemoApp.soundAlarm);
-  $('#my-field').keydown(function(event) {
-    console.log(event.keyCode);
-  });
-});
